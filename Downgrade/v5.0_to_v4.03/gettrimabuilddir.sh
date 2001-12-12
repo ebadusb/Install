83811d336c8372ac4cfa7c28f6f6780d5c02fafa
Scott, Mark <Mark.Scott@terumobct.com>
2001-12-11 16:52:20 -0800

    [git-p4: depot-paths = "//depot/main/embedded/Trima/Install/": change = 3756]

--- Downgrade/v5.0_to_v4.03/gettrimabuilddir.sh
+++ Downgrade/v5.0_to_v4.03/gettrimabuilddir.sh
@@ -4,7 +4,7 @@
 #
 
 if [ ! -f latest_trima_build_dir.mk ]; then
-        cat /bctdev/BCT_Development/TrimaV4/Triam/trima_builds/latest_trima_build_dir.mk; 
+        cat /bctdev/BCT_Development/TrimaV4/Trima/trima_builds/latest_trima_build_dir.mk; 
 else 
         cat latest_trima_build_dir.mk;
 fi
